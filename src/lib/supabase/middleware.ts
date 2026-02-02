9b3d7a431ba001602e22e53d16268282dba28a89
TommieFish <tommiejfish@gmail.com>
2026-02-02 17:10:41 +0000

    middleware to allow tests

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -15,6 +15,11 @@ export async function updateSession(request: NextRequest)
       setAll(cookiesToSet)
       {
         cookiesToSet.forEach((cookie) => {
+          if (cookie.name.startsWith("sb-") && cookie.name.endsWith("-auth-token")) 
+            { 
+              return;
+            }
+          
           req.cookies.set(cookie.name, cookie.value);
           res.cookies.set(cookie.name, cookie.value, cookie.options);
         })
